f230b012f4557e55bd45f1d37dd887677b1f9939
Arthur Martinez <martinez.arthur2602@gmail.com>
2022-07-27 23:15:24 +0200

    Correction beug quantité produit premier prenait la valeur du modif

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,7 @@
 //*********TOUS LES COMMENTAIRES DECRIVENT LE CODE EN DESSOUS D'EUX*********//
 //Vérifier que les données ont bien été récupérées
 let getLocalStorage = JSON.parse(localStorage.getItem("Basket"));
-let modifyQuantityItemBtn = document.getElementsByClassName('itemQuantity');
+let buttonGetForm = document.getElementById("order");
 //Appel de l'API et récupération des données, erreur en cas d'echec de la requete
 fetch('http://localhost:3000/api/products')
 .then(function (response) {
@@ -9,9 +9,87 @@ fetch('http://localhost:3000/api/products')
 })
 .then(function (data) {
     getItemCart(data);
-    deleteItem(data);
-    modifyQuantityItem(data);
-})
+    deleteItem();    
+    modifyQuantityItem();
+    //Au clic on vérifie si le panier n'est pas vide
+    //Si il y a au moins un article, on enregistre les données du formulaire sur le local storage
+    buttonGetForm.addEventListener("click", (e) => { 
+        e.preventDefault()
+        //Définir les éléments du formulaire dans le dom
+        let inputName = document.getElementById('firstName').value;
+        let inputLastName = document.getElementById('lastName').value;
+        let inputAdress = document.getElementById('address').value;
+        let inputCity = document.getElementById('city').value;
+        let inputMail = document.getElementById('email').value;
+        //Vérification validation du formulaire
+        if(positionEmptyCart.innerHTML === emptyCart){
+            window.alert("Vous n'avez aucun article dans le panier");
+        }else{    
+            let formContent = {
+                    firstName : inputName,
+                    lastName : inputLastName,
+                    address : inputAdress,
+                    city : inputCity,
+                    email : inputMail,
+                }
+            //Vérification données envoyées avec RegEx
+            //Ajout des données du formulaire dans le Local storage et execution de la fonction sendOrder
+            if( /^[a-z ,.'-]+$/i.test(formContent.firstName) &&
+                /^[a-z ,.'-]+$/i.test(formContent.lastName) &&
+                /^[a-z ,.'-]+$/i.test(formContent.city) &&
+                /^[A-Za-z0-9'\.\-\s\,]+$/i.test(formContent.address) &&
+                /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i.test(formContent.email))
+                {
+                console.log("Ok");       
+                console.log(formContent);
+                localStorage.setItem("Form Info", JSON.stringify(formContent));
+                sendOrder();
+            }else{
+                window.alert("Veuillez correctement remplir le formulaire")
+            };
+        }
+    })
+    //Envoie du formulaire et récupération de la réponse de l'API
+    function sendOrder(){
+        let inputName = document.getElementById('firstName').value;
+        let inputLastName = document.getElementById('lastName').value;
+        let inputAdress = document.getElementById('address').value;
+        let inputCity = document.getElementById('city').value;
+        let inputMail = document.getElementById('email').value;
+        //Récupération de tous les Id produits dans le local storage
+        let productIdValues = getLocalStorage.map((item) => item.idProduct);  
+        //Execution du paramètre POST de l'API 
+        fetch ("http://localhost:3000/api/products/order", {
+            method: "POST",    
+            headers: {
+                Accept: "application/json",
+                "Content-Type" : "application/json",
+            },        
+            body: JSON.stringify({
+                contact: {
+                firstName: inputName,
+                lastName: inputLastName,
+                address: inputAdress,
+                city: inputCity,
+                email: inputMail,
+                },
+                products: productIdValues,
+            })
+        })
+        .then(function (response) {
+            return response.json()
+        })
+        //Local storage vidé et redirection vers la page confirmation
+        .then(function (data) {
+            console.log(data)
+            localStorage.clear();
+            document.location.href = `confirmation.html?id=${data.orderId}`;
+    })
+    .catch(function (error) {
+        console.log('Pas de liaison effectué', error)
+    })
+}
+})    
 .catch(function (error) {
     console.log('Pas de liaison effectué', error);
 })
@@ -27,22 +105,20 @@ function getCart(){
         positionEmptyCart.innerHTML = emptyCart;  
     //Affiche les éléments du panier sur la page
     } else {
-        getItemCart(data);    
+        getItemCart();    
     }
-    deleteItem(data);
-    modifyQuantityItem(data);
 }
 getCart();
 //Implémenter les éléments récupérés de la page 'produit' dans le DOM
 //Le prix vient directement de l'API pour des raisons de sécurité
 function getItemCart(data) {
     for (let i = 0; i < data.length; i++) {
-        let dataPriceAll = data[i]._id
+        let dataIdAll = data[i]._id
         for (let Basket in getLocalStorage) {
-            if(getLocalStorage[Basket].idProduct === dataPriceAll){
+            if(getLocalStorage[Basket].idProduct === dataIdAll){
                 console.log(data)
                     const html = `
-                        <article class="cart__item" data-id="${dataPriceAll}" data-color="${getLocalStorage[Basket].colorsChoice}">
+                        <article class="cart__item" data-id="${dataIdAll}" data-color="${getLocalStorage[Basket].colorsChoice}">
                             <div class="cart__item__img">
                                 <img src="${getLocalStorage[Basket].productImg}" alt="${getLocalStorage[Basket].productImgAlt}">
                             </div>
@@ -88,16 +164,30 @@ function getItemCart(data) {
         }
     }
 }
+//Modification quantité produit 
+function modifyQuantityItem() {
+    //Boucle pour appliquer la commande à tous les boutons modifier la quantité
+    let modifyQuantityItemBtn = document.getElementsByClassName('itemQuantity');
+    for (let i = 0; i < modifyQuantityItemBtn.length; i++){
+        modifyQuantityItemBtn[i].addEventListener("change", (stop) => {
+            stop.preventDefault();            
+            let newQuantity = modifyQuantityItemBtn[i].valueAsNumber;
+            getLocalStorage[i].productNumber = newQuantity
 
+            localStorage.setItem("Basket", JSON.stringify(getLocalStorage));
+            location.reload();
+        }
+    )}
+}
 //Suppression du produit
-function deleteItem(data){
+function deleteItem(){
     //Boucle pour appliquer la commande à tous les boutons supprimer
     let deleteItemBtn = document.getElementsByClassName("deleteItem");
-    for (let e = 0; e < deleteItemBtn.length; e++){
-        deleteItemBtn[e].addEventListener("click", (stop) => {
+    for (let i = 0; i < deleteItemBtn.length; i++){
+        deleteItemBtn[i].addEventListener("click", (stop) => {
             stop.preventDefault();
-            let idDelete = getLocalStorage[e].idProduct;
-            let colorDelete = getLocalStorage[e].colorsChoice;
+            let idDelete = getLocalStorage[i].idProduct;
+            let colorDelete = getLocalStorage[i].colorsChoice;
             getLocalStorage = getLocalStorage.filter(p => p.idProduct != idDelete || p.colorsChoice != colorDelete);
             localStorage.setItem("Basket", JSON.stringify(getLocalStorage));
             location.reload();
@@ -105,98 +195,5 @@ function deleteItem(data){
     
     }
 }
-//Modification quantité produit 
-function modifyQuantityItem(data) {
-    //Boucle pour appliquer la commande à tous les boutons modifier la quantité
-    for (let i = 0; i < modifyQuantityItemBtn.length; i++){
-        modifyQuantityItemBtn[i].addEventListener("change", (stop) => {
-            stop.preventDefault();
-            let modifyQuantity = getLocalStorage[i].productNumber;
-            let newQuantity = modifyQuantityItemBtn[i].valueAsNumber;
-            let resultFind = getLocalStorage.find((i) => i.newQuantity != modifyQuantity);
-            resultFind.productNumber = newQuantity;
-            getLocalStorage[i].productNumber = resultFind.productNumber;
-            localStorage.setItem("Basket", JSON.stringify(getLocalStorage));
-            location.reload();
-        }
-    )}
-}
-let buttonGetForm = document.getElementById("order");
-//Au clic on vérifie si le panier n'est pas vide
-//Si il y a au moins un article, on enregistre les données du formulaire sur le local storage
-buttonGetForm.addEventListener("click", (e) => { 
-    e.preventDefault()
-    //Définir les éléments du formulaire dans le dom
-    let inputName = document.getElementById('firstName').value;
-    let inputLastName = document.getElementById('lastName').value;
-    let inputAdress = document.getElementById('address').value;
-    let inputCity = document.getElementById('city').value;
-    let inputMail = document.getElementById('email').value;
-    //Vérification validation du formulaire
-    if(positionEmptyCart.innerHTML === emptyCart){
-        window.alert("Vous n'avez aucun article dans le panier");
-     }else{    
-        let formContent = {
-                firstName : inputName,
-                lastName : inputLastName,
-                address : inputAdress,
-                city : inputCity,
-                email : inputMail,
-            }
-        //Vérification données envoyées avec RegEx
-        //Ajout des données du formulaire dans le Local storage et execution de la fonction sendOrder
-        if( /^[a-z ,.'-]+$/i.test(formContent.firstName) &&
-            /^[a-z ,.'-]+$/i.test(formContent.lastName) &&
-            /^[a-z ,.'-]+$/i.test(formContent.city) &&
-            /^[A-Za-z0-9'\.\-\s\,]+$/i.test(formContent.address) &&
-            /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i.test(formContent.email))
-            {
-            console.log("Ok");       
-            console.log(formContent);
-            localStorage.setItem("Form Info", JSON.stringify(formContent));
-            sendOrder();
-        }else{
-            window.alert("Veuillez correctement remplir le formulaire")
-        };
-    }
-})
-//Envoie du formulaire et récupération de la réponse de l'API
-function sendOrder(){
-    let inputName = document.getElementById('firstName').value;
-    let inputLastName = document.getElementById('lastName').value;
-    let inputAdress = document.getElementById('address').value;
-    let inputCity = document.getElementById('city').value;
-    let inputMail = document.getElementById('email').value;
-    //Récupération de tous les Id produits dans le local storage
-    let productIdValues = getLocalStorage.map((item) => item.idProduct);  
-    //Execution du paramètre POST de l'API 
-    fetch ("http://localhost:3000/api/products/order", {
-        method: "POST",    
-        headers: {
-            Accept: "application/json",
-            "Content-Type" : "application/json",
-        },        
-        body: JSON.stringify({
-            contact: {
-              firstName: inputName,
-              lastName: inputLastName,
-              address: inputAdress,
-              city: inputCity,
-              email: inputMail,
-            },
-            products: productIdValues,
-        })
-    })
-    .then(function (response) {
-        return response.json()
-    })
-    //Local storage vidé et redirection vers la page confirmation
-    .then(function (data) {
-        console.log(data)
-        localStorage.clear();
-        document.location.href = `confirmation.html?id=${data.orderId}`;
-    })
-    .catch(function (error) {
-        console.log('Pas de liaison effectué', error)
-    })
-}
+
+
